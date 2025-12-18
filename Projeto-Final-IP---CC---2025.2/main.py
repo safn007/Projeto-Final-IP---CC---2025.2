737c609c3f1cbb7f7eeb5b428e0ef5051b42e574
Samuel Nascimento <safn@cin.ufpe.br>
2025-12-17 22:08:53 -0300

    inseri tela_inicial.py e instrucoes.py na pasta src / arquivo main.py voltou a rodar o cód principal.

--- Projeto-Final-IP---CC---2025.2/main.py
+++ Projeto-Final-IP---CC---2025.2/main.py
@@ -1,86 +1,92 @@
-#teste Samuel comentário
+
 import pygame
 import sys
 from src.mapas import Mapas
 from src.colisoes import Colisoes
 from src.coletaveis import Coletavel
 from src.player import Player
-#salvando mudanças na main
-def executar_jogo():
-    pygame.init() # Inicia o pygame
-
-    largura = 960
-    altura = 640
-    mapas = Mapas(largura, altura)
-    tela = pygame.display.set_mode((largura, altura)) # Define o tamanho da janela do jogo
-    pygame.display.set_caption("Ricardo Jones: Uma Aventura MangueBeat") # Nome que aparece no título da janela
-
-    player = Player(250, 250)
-
-    grupo_coletaveis = pygame.sprite.Group() #criando os coletaveis
-    #adicionando no mapa
-    grupo_coletaveis.add(Coletavel("chapeu", 100, 100))
-    grupo_coletaveis.add(Coletavel("oculos", 150, 100))
-    grupo_coletaveis.add(Coletavel("carangueijo", 600, 500))
-    grupo_coletaveis.add(Coletavel("carangueijo", 400, 100))
-
-    #variaveis para contagem de coletaveis
-    qnt_chapeu = 0
-    qnt_oculos = 0
-    qnt_carangueijo = 0
-
-    # Grupo de sprites
-    sprites_group = pygame.sprite.Group()
-    sprites_group.add(player)
-
-    clock = pygame.time.Clock() # Cria relogio pra controlar FPS
-    rodando_jogo = True
-
-    while rodando_jogo:
-        clock.tick(60) # Limita os FPS a 60
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.QUIT:
-                rodando_jogo = False
+from src.tela_inicial import menu_principal
 
 
-        for item in grupo_coletaveis:
-            if player.hitbox.colliderect(item.rect):
-                item.kill() #remove o item do jogo e do grupo
+pygame.init() # Inicia o pygame
 
-                if item.tipo == "chapeu":
-                    print("pegou chapeu")
-                    qnt_chapeu+=1
-                elif item.tipo == "oculos":
-                    print("pegou oculos")
-                    qnt_oculos+=1
-                elif item.tipo == "carangueijo":
-                    print("pegou carangueijo")
-                    qnt_carangueijo+=1
-                    
-        # desenha mapa
-        mapas.desenhar(tela)
+largura = 960
+altura = 640
+mapas = Mapas(largura, altura)
+tela = pygame.display.set_mode((largura, altura)) # Define o tamanho da janela do jogo
 
-        # troca de mapa (altera o rect diretamente)
-        mapas.trocar_mapa(player.rect)
+#janela Menu
+menu_principal()
 
-        # coletar colisões
-        colisoes = Colisoes([3, 5, 9, 11, 241])
-        colisoes = colisoes.criar_colisoes()
-        for coord in colisoes:
-            caixa = pygame.Rect(coord[0], coord[1], 32, 32)
+#Janela Jogo
+pygame.display.set_caption("Ricardo Jones: Uma Aventura MangueBeat") # Nome que aparece no título da janela
 
-        # Atualiza todos os sprites do grupo
-        sprites_group.update() 
-        
-        # Desenha os sprites na janela
-        sprites_group.draw(tela)
-        grupo_coletaveis.draw(tela) #desenha os coletaveis
+player = Player(250, 250)
+
+grupo_coletaveis = pygame.sprite.Group() #criando os coletaveis
+#adicionando no mapa
+grupo_coletaveis.add(Coletavel("chapeu", 100, 100))
+grupo_coletaveis.add(Coletavel("oculos", 150, 100))
+grupo_coletaveis.add(Coletavel("carangueijo", 600, 500))
+grupo_coletaveis.add(Coletavel("carangueijo", 400, 100))
+
+#variaveis para contagem de coletaveis
+qnt_chapeu = 0
+qnt_oculos = 0
+qnt_carangueijo = 0
+
+# Grupo de sprites
+sprites_group = pygame.sprite.Group()
+sprites_group.add(player)
+
+clock = pygame.time.Clock() # Cria relogio pra controlar FPS
+rodando_jogo = True
+
+while rodando_jogo:
+    clock.tick(60) # Limita os FPS a 60
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.QUIT:
+            rodando_jogo = False
+
+
+    for item in grupo_coletaveis:
+        if player.hitbox.colliderect(item.rect):
+            item.kill() #remove o item do jogo e do grupo
+
+            if item.tipo == "chapeu":
+                print("pegou chapeu")
+                qnt_chapeu+=1
+            elif item.tipo == "oculos":
+                print("pegou oculos")
+                qnt_oculos+=1
+            elif item.tipo == "carangueijo":
+                print("pegou carangueijo")
+                qnt_carangueijo+=1
+                
+    # desenha mapa
+    mapas.desenhar(tela)
+
+    # troca de mapa (altera o rect diretamente)
+    mapas.trocar_mapa(player.rect)
+
+    # coletar colisões
+    colisoes = Colisoes([3, 5, 9, 11, 241])
+    colisoes = colisoes.criar_colisoes()
+    for coord in colisoes:
+        caixa = pygame.Rect(coord[0], coord[1], 32, 32)
+
+    # Atualiza todos os sprites do grupo
+    sprites_group.update() 
     
-        
-        pygame.display.update()
+    # Desenha os sprites na janela
+    sprites_group.draw(tela)
+    grupo_coletaveis.draw(tela) #desenha os coletaveis
+
+    
+    pygame.display.update()
 
     
---
+++ Projeto-Final-IP---CC---2025.2/src/instrucoes.py
@@ -0,0 +1,42 @@
+import pygame
+import os
+images_path_base = os.path.join("Assets", "Imagens")
+
+
+def mostrar_instrucao():
+    pygame.init()
+    LARGURA, ALTURA = 960, 640
+    tela = pygame.display.set_mode((LARGURA, ALTURA))
+    pygame.display.set_caption("Tela de Instruções")
+
+    #plano de fundo
+    try:
+        imagem_fundo = pygame.image.load(os.path.join(images_path_base, 'plano_de_fundo_instrucoes_oficial.2.webp')).convert()
+        imagem_fundo = pygame.transform.scale(imagem_fundo, (LARGURA, ALTURA))
+    
+    except:
+        imagem_fundo = pygame.Surface((LARGURA, ALTURA))
+        imagem_fundo.fill((75, 83, 32))
+
+
+    # 4. Configurações do Botão
+    largura_btn, altura_btn = 220, 60
+    # Criamos o retângulo centralizado
+    botao_rect = pygame.Rect(((LARGURA//2 - largura_btn//2) - 3, ALTURA//2 + 80, largura_btn, altura_btn))
+
+    # 5. Loop Principal
+    while True:
+        tela.blit(imagem_fundo, (0, 0))
+        
+        # Posição do mouse
+        mouse_pos = pygame.mouse.get_pos()
+        
+        # Eventos
+        for evento in pygame.event.get():
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                if botao_rect.collidepoint(mouse_pos):
+                    return
+
+        pygame.display.flip()
+
+
