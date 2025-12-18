a10602635618ee8ad967a60d82e4921e9e377ba1
Samuel Nascimento <safn@cin.ufpe.br>
2025-12-17 23:58:25 -0300

    commit 1 novo arquivo adição tela_inicial.py e instrucoes.py

---
+++ src/mapas.py
@@ -0,0 +1,53 @@
+import pygame
+import os
+imagens_path = os.path.join ('Assets', 'Imagens')
+
+class Mapas:
+    def __init__(self, largura, altura):
+        self.largura = largura
+        self.altura = altura
+
+        self.mapa1 = pygame.image.load(os.path.join(imagens_path, 'mapa_1.png'))
+        self.mapa2 = pygame.image.load(os.path.join(imagens_path, 'mapa_2.png'))
+        self.mapa3 = pygame.image.load(os.path.join(imagens_path, 'mapa_3.png'))
+
+        self.mapa1 = pygame.transform.scale(self.mapa1, (largura, altura))
+        self.mapa2 = pygame.transform.scale(self.mapa2, (largura, altura))
+        self.mapa3 = pygame.transform.scale(self.mapa3, (largura, altura))
+
+        # mapa atual
+        self.mapa_atual = 1
+
+    def desenhar(self, tela):
+        if self.mapa_atual == 1:
+            tela.blit(self.mapa1, (0, 0))
+        elif self.mapa_atual == 2:
+            tela.blit(self.mapa2, (0, 0))
+        elif self.mapa_atual == 3:
+            tela.blit(self.mapa3, (0, 0))
+
+    def trocar_mapa(self, player):
+
+        if self.mapa_atual == 1:
+            if player.rect.top > self.altura:
+                self.mapa_atual = 2
+                player.rect.y = 0
+                player.pos_y = 0.0
+
+        elif self.mapa_atual == 2:
+            if player.rect.bottom < 0:
+                self.mapa_atual = 1
+                player.rect.y = self.altura - player.rect.height
+                player.pos_y = float(player.rect.y)
+
+            elif player.rect.left > self.largura:
+                self.mapa_atual = 3
+                player.rect.x = 0
+                player.pos_x = 0.0
+
+        elif self.mapa_atual == 3:
+            if player.rect.right < 0:
+                self.mapa_atual = 2
+                player.rect.x = self.largura - player.rect.width
+                player.pos_x = float(player.rect.x)
+
---
+++ src/tela_inicial.py
@@ -0,0 +1,66 @@
+import pygame
+import os
+import sys
+
+
+imagens_path = os.path.join("Assets", "Imagens")
+
+pygame.init()
+
+# Configurações da Janela
+LARGURA, ALTURA = 960, 640
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption("Meu Jogo - Tela Inicial")
+
+#imagem do fundo
+imagem_fundo = pygame.image.load(os.path.join(imagens_path, 'plano_de_fundo_tela_inicial.webp'))
+imagem_fundo = pygame.transform.scale(imagem_fundo, (LARGURA, ALTURA))
+
+# Cores
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+AZUL = (50, 150, 200)
+VERDE = (50, 200, 150)
+COR_TEXTO = (255, 255, 255)
+
+# Fontes
+fonte_titulo = pygame.font.SysFont("Arial", 64, bold=True)
+fonte_subtitulo = pygame.font.SysFont("Arial", 54, bold=True)
+fonte_botao = pygame.font.SysFont("Arial", 32)
+
+def desenhar_texto(texto, fonte, cor, x, y):
+    img = fonte.render(texto, True, cor)
+    rect = img.get_rect(center=(x, y))
+    tela.blit(img, rect)
+
+
+def menu_principal():
+    rodando = True
+    while rodando:
+        tela.blit(imagem_fundo, (0, 0)) # Fundo da tela
+        
+        # Posicionamento do Mouse
+        mouse_pos = pygame.mouse.get_pos()
+
+        # Definição dos Retângulos dos Botões
+        btn_iniciar = pygame.Rect(LARGURA // 2 - 105, 320, 200, 50)
+        btn_instrucoes = pygame.Rect(LARGURA // 2 - 105, 385, 200, 50)
+
+
+        # Tratamento de Eventos
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                return
+            
+            elif evento.type == pygame.MOUSEBUTTONDOWN:
+                if btn_iniciar.collidepoint(mouse_pos):
+                    return
+                
+                elif btn_instrucoes.collidepoint(mouse_pos):
+                    from src.instrucoes import mostrar_instrucao
+                    mostrar_instrucao()
+
+        
+        pygame.display.update()
